4bb0c7f8474cda29858ab7446117940cb22fcec1
Menake Dasunpriya <menakedasunpriya@gmail.com>
2022-01-18 23:52:31 +0530

    fix: input tag color fix

--- components/common/InputTag.jsx
+++ components/common/InputTag.jsx
@@ -14,7 +14,7 @@ const InputTag = ({
         type={type}
         placeholder={placeholder}
         {...register(name, validation)}
-        className={`px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring ${width} `}
+        className={`px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-gray-50  rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring ${width} `}
       />
       {errors[name] && (
         <p className={`absolute -bottom-5 right-0 text-xs text-red-600`}>
